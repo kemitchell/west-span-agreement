be2f23c071d5da6b20960305a42afccc2feed4f3
Kyle Mitchell <kyle@kemitchell.com>
2015-05-18 23:08:22 +0000

    Check for copies of referenced sources

--- tests/scenarios.js
+++ tests/scenarios.js
@@ -35,29 +35,47 @@ var validScenario = function(argument) {
   );
 };
 
-glob('scenarios/**.yaml', function(error, scenarioFiles) {
-  var foundProblem = false;
-  async.each(
-    scenarioFiles,
-    function(scenarioFile) {
-      var buffer = '';
-      fs.createReadStream(scenarioFile)
-        .on('data', function(data) {
-          buffer += data;
-        })
-        .on('end', function() {
-          var scenario = yaml.load(buffer);
-          if (!validScenario(scenario)) {
-            foundProblem = true;
-            console.error(
-              '"' + path.basename(scenarioFile, '.yaml') +
-              '" is not a complete scenario.'
-            );
-          }
-        })
-    },
-    function(error) {
-      process.exit(foundProblem ? 1 : 0);
-    }
-  );
+glob('references/**.md', function(error, referenceFiles) {
+  var referenceNames = referenceFiles
+    .map(function(fileName) {
+      return path.basename(fileName, '.md');
+    });
+  glob('scenarios/**.yaml', function(error, scenarioFiles) {
+    var foundProblem = false;
+    async.each(
+      scenarioFiles,
+      function(scenarioFile) {
+        var buffer = '';
+        fs.createReadStream(scenarioFile)
+          .on('data', function(data) {
+            buffer += data;
+          })
+          .on('end', function() {
+            var scenario = yaml.load(buffer);
+            if (!validScenario(scenario)) {
+              foundProblem = true;
+              console.error(
+                '"' + path.basename(scenarioFile, '.yaml') + '"' +
+                ' is not a complete scenario.'
+              );
+            } else {
+              scenario.references
+                .forEach(function(scenarioName) {
+                  if (referenceNames.indexOf(scenarioName) < 0) {
+                    foundProblem = true;
+                    console.error(
+                      '"' + path.basename(scenarioFile, '.yaml') + '"' +
+                      ' refers to missing source ' +
+                      '"' + scenarioName + '".'
+                    );
+                  }
+                });
+            }
+          })
+      },
+      function(error) {
+        process.exit(foundProblem ? 1 : 0);
+      }
+    );
+  });
 });
